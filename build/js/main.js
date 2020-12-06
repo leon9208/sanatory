83f1661c12aa2d2e7f8bd857ffeba9c17dfa9216
Shakhrom <leon9208@mail.ru>
2020-12-06 18:40:00 +0300

    Start project

---
+++ build/js/main.js
@@ -0,0 +1,30 @@
+
+//fix hover element on mobile
+let allEl = document.querySelectorAll('*')
+for(let i = 0; i < allEl.length; i++){
+  allEl.ontouchstart = () => this.mouseover()
+  allEl.ontouchcancel = () => this.mouseover()
+}
+
+//Проверка поддержки WEBP
+function check_webp_feature(feature, callback) {
+  var kTestImages = {
+      lossy: "UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA",
+      lossless: "UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA==",
+      alpha: "UklGRkoAAABXRUJQVlA4WAoAAAAQAAAAAAAAAAAAQUxQSAwAAAARBxAR/Q9ERP8DAABWUDggGAAAABQBAJ0BKgEAAQAAAP4AAA3AAP7mtQAAAA==",
+      animation: "UklGRlIAAABXRUJQVlA4WAoAAAASAAAAAAAAAAAAQU5JTQYAAAD/////AABBTk1GJgAAAAAAAAAAAAAAAAAAAGQAAABWUDhMDQAAAC8AAAAQBxAREYiI/gcA"
+  };
+  var img = new Image();
+  img.onload = function () {
+      var result = (img.width > 0) && (img.height > 0);
+      callback(feature, result);
+  };
+  img.onerror = () => callback(feature, false);
+  img.src = "data:image/webp;base64," + kTestImages[feature];
+}
+//Если браузер поддерживает WEBP, то добавляем класс тегу body
+check_webp_feature('lossy', function (feature, isSupported) {
+  isSupported ? document.body.classList.add('webp-support-js') : document.body.classList.add('webp-nosupport-js')
+});
+//hero
+//nav
